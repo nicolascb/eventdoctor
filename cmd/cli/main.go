2857fad5658e52aab4635875cfe501d80050518d
Nicolas Barbosa <ndevbarbosa@gmail.com>
2025-10-28 10:06:54 -0300

    chore: wip commands

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,23 +1,64 @@
 package main
 
 import (
-	"database/sql"
+	"context"
+	"fmt"
+	"log"
 	"os"
 
-	"go.uber.org/zap"
+	"github.com/urfave/cli/v3"
 )
 
 func main() {
-}
-
-func handleShutdown(quit chan os.Signal, db *sql.DB, logger *zap.Logger) {
-	<-quit
-	logger.Info("shutting down server gracefully...")
-
-	if err := db.Close(); err != nil && err != sql.ErrConnDone {
-		logger.Error("error closing database", zap.Error(err))
+	cmd := &cli.Command{
+		// Arguments: []cli.Argument{
+		// 	&cli.StringArgs{
+		// 		Name:      "file",
+		// 		UsageText: "path to the configuration file",
+		// 	},
+		// },
+		Commands: commands(),
 	}
 
-	logger.Info("server stopped successfully")
-	logger.Sync()
+	if err := cmd.Run(context.Background(), os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func commands() []*cli.Command {
+	return []*cli.Command{
+		{
+			Name:      "config",
+			Aliases:   []string{"c"},
+			Usage:     "options for config",
+			ArgsUsage: "args usage",
+			Arguments: []cli.Argument{
+				&cli.StringArgs{
+					Name:      "file",
+					UsageText: "the config file path",
+					Min:       0,
+					Max:       -1,
+				},
+			},
+			Commands: []*cli.Command{
+				{
+					Name:    "apply",
+					Aliases: []string{"a"},
+					Usage:   "Apply EventDoctor config",
+					Flags: []cli.Flag{
+						&cli.StringFlag{
+							Required: true,
+							Name:     "api-url",
+							Usage:    "The EventDoctor API URL",
+							Aliases:  []string{"u"},
+						},
+					},
+					Action: func(ctx context.Context, cmd *cli.Command) error {
+						fmt.Println("Apply config", cmd.String("api-url"))
+						return nil
+					},
+				},
+			},
+		},
+	}
 }
--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/goccy/go-yaml v1.18.0
 	github.com/joeshaw/envdecode v0.0.0-20200121155833-099f1fc765bd
 	github.com/mattn/go-sqlite3 v1.14.32
+	github.com/urfave/cli/v3 v3.5.0
 	go.uber.org/zap v1.27.0
 )
 
