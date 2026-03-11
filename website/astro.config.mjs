cef31eb237925b5c71d72b032bd452d944031dba
Nicolas Barbosa <ndevbarbosa@gmail.com>
2026-03-11 11:30:37 -0300

    fix (website): base link url

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://nicolascb.github.io',
+	site: 'https://nicolascb.github.io/',
 	base: '/eventdoctor',
 	integrations: [
 		starlight({
